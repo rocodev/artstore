46080642011cea10c9d1955ab161edbe8ec3454c
xdite <xuite.joke@gmai.com>
2014-06-17 19:49:10 +0800

    implement clear cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
       @order.build_item_cache_from_cart(current_cart)
       @order.calculate_total!(current_cart)
       
+      current_cart.clear!
       OrderMailer.notify_order_placed(@order).deliver
 
       redirect_to order_path(@order.token)
