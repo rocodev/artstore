b6c854e7c27bd8a896573b00f9ce9c3a4cfc98f6
xdite <xuite.joke@gmai.com>
2014-06-17 20:17:53 +0800

    update cart_item_quantity

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,6 +2,17 @@ class CartItemsController < ApplicationController
 
   before_action :authenticate_user!
 
+ # before_action :find_cart
+
+  def update
+    @cart = current_cart
+    @item = @cart.cart_items.find(params[:id])
+
+    @item.update(item_params)
+    
+    redirect_to carts_path
+  end
+
   def destroy
     @cart = current_cart
     @item = @cart.cart_items.find(params[:id])
@@ -12,4 +23,12 @@ class CartItemsController < ApplicationController
     redirect_to :back
 
   end
+
+
+  private
+
+  def item_params
+    params.require(:cart_item).permit(:quantity)
+  end
+
 end
