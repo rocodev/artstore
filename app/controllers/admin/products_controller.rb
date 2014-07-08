18069a533e10dd3175a8e1910069eff3cb705c24
edcckao <edcckao@gmail.com>
2014-07-08 20:49:25 +0800

    Add products index and rake db:migrate (w1r2p17)

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,21 +1,25 @@
 class Admin::ProductsController < ApplicationController
- 
+
   def new
     @product = Product.new
   end
- 
+
   def create
     @product = Product.new(product_params)
- 
+
     if @product.save
       redirect_to admin_products_path
     else
       render :new
     end
   end
- 
+
+  def index
+    @products = Product.all
+  end
+
   private
- 
+
   def product_params
     params.require(:product).permit(:title, :description,:quantity)
   end
