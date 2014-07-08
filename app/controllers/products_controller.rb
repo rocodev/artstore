fd5242e60c5a1d2b39a7350115cce62aaa8fca63
nkj20932 <nkj20932@hotmail.com>
2014-07-09 00:25:13 +0800

    add products controller with index & show action

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,10 @@
+class ProductsController < ApplicationController
+  def index
+  	@product = Product.all
+  end
+
+  def show
+  	@product = Product.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-
-  root to: "admin/products#index"
+  resources :products
+  root to: "products#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
