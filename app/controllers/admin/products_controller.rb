aca7ee058aefaf05f442c0f145ca1191a426aeab
edcckao <edcckao@gmail.com>
2014-07-10 03:34:30 +0800

    Add CRUD for Products except show

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,6 +3,7 @@ class Admin::ProductsController < ApplicationController
 before_action :authenticate_user!
 before_action :admin_required
 
+# CREATE
   def new
     @product = Product.new
   end
@@ -17,8 +18,36 @@ before_action :admin_required
     end
   end
 
+# READ
   def index
     @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+# UPDATE
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to admin_products_path(@product)
+    else
+      render :edit
+    end
+  end
+
+# DESTROY
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = "刪除成功"
+    redirect_to admin_products_path
   end
 
   private
