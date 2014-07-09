ee0c4cf0e22173f354f96fbbeb384af260b07af9
BobLin <bob76828@gmail.com>
2014-07-09 20:47:06 +0800

    ADD:加入購物車按鈕,後台連結

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,14 @@ module ApplicationHelper
   def frontEndHeaders
     @enable_nav = true
     @headers = [{:title=>"我的購物車",:link=>"/"},{:title=>"我的訂單",:link=>"/"}]
+    addBackendLink if user_signed_in?
   end
   def backEndHeaders
     @enable_nav = true
     @headers = [{:title=>"商品上架",:link=>"/back_end"}]
+  end
+
+  def addBackendLink
+    @headers.push({:title=>"後台",:link=>"/back_end"}) if current_user.admin?
   end
 end
