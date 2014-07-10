74b1516b56420f954c1b5ab9e0f377e6117c193b
Jimmy <jimmy0328@gmail.com>
2014-07-10 19:43:48 +0800

    add carrierwave Gem and mini_magick Gem

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -24,7 +24,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title,:description,:quantity)
+    params.require(:product).permit(:title,:description,:quantity, :image)
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710094701) do
+ActiveRecord::Schema.define(version: 20140710113528) do
 
   create_table "products", force: true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140710094701) do
     t.integer  "quantity"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "users", force: true do |t|
