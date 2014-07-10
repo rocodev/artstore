a749531adfcac53522a3fbb656ae01ebc1f11a5c
Gary.Chen <gary.chen@drsays.org>
2014-07-10 21:39:09 +0800

    add user auth

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
-
+	before_action :authenticate_user!
+	before_action :admin_required
+	
 	def index 
 		@products = Product.all
 	end
@@ -33,7 +35,7 @@ class Admin::ProductsController < ApplicationController
 			render :new
 		end
 	end 
-	
+
 	def update 
 
 		@product = Product.find(params[:id])
