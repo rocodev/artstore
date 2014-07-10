3180f4882216219be9f243433b1712ab336cf695
sdlong <sdlong.jeng@gmail.com>
2014-07-10 13:10:10 +0800

    def controller admin_products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,38 @@
 class Admin::ProductsController < ApplicationController
+ 
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :quantity)
+  end
 end
