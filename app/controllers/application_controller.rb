4ee5513869bcba46f8530b4006f3bf90544d8eae
sdlong <sdlong.jeng@gmail.com>
2014-07-10 14:30:49 +0800

    修改 admin_required 的bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def admin_required 
-    current_user.admin?
+  def admin_required
+    if !current_user.admin?
+      redirect_to root_path, alert: 'you are not admin!!'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+  root :to => "products#index"
+
   devise_for :users
   namespace :admin do
     resources :products
   end
+
 end
