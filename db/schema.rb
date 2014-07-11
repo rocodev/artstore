9f9f91a9ec7fe02c7d442180f23c868bf9823088
Jimmy <jimmy0328@gmail.com>
2014-07-11 14:19:24 +0800

    alter table  price and quantity column

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710113528) do
+ActiveRecord::Schema.define(version: 20140711060601) do
 
   create_table "products", force: true do |t|
     t.string   "title"
     t.text     "description"
-    t.integer  "quantity"
+    t.integer  "quantity",    default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
+    t.float    "price",       default: 0.0
   end
 
   create_table "users", force: true do |t|
