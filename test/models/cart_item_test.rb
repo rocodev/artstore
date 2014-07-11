51fea00e86ee92cc8683ef926b3649bf7ccc8c26
sdlong <sdlong.jeng@gmail.com>
2014-07-11 13:30:46 +0800

    user annotate to show database table column

--- test/models/cart_item_test.rb
+++ test/models/cart_item_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: cart_items
+#
+#  id         :integer          not null, primary key
+#  cart_id    :integer
+#  product_id :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'test_helper'
 
 class CartItemTest < ActiveSupport::TestCase
