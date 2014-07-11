da5a68f9d5c70243ecb8c3c5754d500f64c8a6d2
Jimmy <jimmy0328@gmail.com>
2014-07-11 23:37:20 +0800

    refactor simple_form_for ui and resize image size

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -16,6 +16,20 @@ class Admin::ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+
   def destroy
     @product = Product.find(params[:id])
     @product.destroy!
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -51,21 +51,17 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [220, 220]
   end
 
-  version :md1 do
-    process resize_to_fill: [300, 300]
-  end
-
-
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "image.jpg" if original_filename
+  end
+
 
 end
