7618ce434e2fc75fa241d081b45a3067f5d1ecbc
playpool513 <playpool513@gmail.com>
2014-07-13 22:10:18 +0800

    build the back end

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  namespace :admin do
+    resources :products
+  end
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
