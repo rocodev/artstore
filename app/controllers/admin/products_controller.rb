c85a12b1b15b7d16fe44f9859ff2a69d915239a5
playpool513 <playpool513@gmail.com>
2014-07-13 23:28:13 +0800

    authenticate users

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,10 +1,12 @@
 class Admin::ProductsController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
     @products = Product.all
   end
 
-  
+
   def new
     @product = Product.new
   end
