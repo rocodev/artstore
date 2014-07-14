9510711e1b59c668504b661675852717241ca00c
sdlong <sdlong.jeng@gmail.com>
2014-07-14 09:36:38 +0800

    作業: 清空購物⾞ done

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,25 +1,22 @@
 class CartsController < ApplicationController
   before_action :authenticate_user!, :only => [:checkout]
 
- # def index
- #   @products = current_cart.items.all
- #   @cart_items = current_cart.cart_items.all
- #   @cart_infos = @products.zip(@cart_items)
- # end
+ def index
+ end
 
-  #def destroy
-  #end
+ #def destroy
+ #end
 
-  #def clear_cart
-  #  current_cart.items.destroy_all
-  #
-  #  redirect_to :action => "index"
-  #end
+ def clear_cart
+  current_cart.clear!
+ 
+  redirect_to :action => "index"
+ end
 
-  #def clear_item
-  #  CartItem.find(params[:id]).destroy
-  #  redirect_to :action => "index"
-  #end  
+ #def clear_item
+ #  CartItem.find(params[:id]).destroy
+ #  redirect_to :action => "index"
+ #end  
   
   def checkout
     @order = current_user.orders.build
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   resources :carts do
     collection do
       post :checkout
+      delete :clear_cart
     end
   end
 
