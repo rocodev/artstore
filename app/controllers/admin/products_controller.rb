8e0feb61476605826a4042c18832a4e6d6670ba2
playpool513 <playpool513@gmail.com>
2014-07-14 09:45:11 +0800

    add /admin/products/edit & /admin/products/show page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -13,6 +13,11 @@ class Admin::ProductsController < ApplicationController
   end
 
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+
   def create
     @product = Product.new(product_params)
 
@@ -20,6 +25,22 @@ class Admin::ProductsController < ApplicationController
       redirect_to admin_products_path
     else
       render :new
+    end
+  end
+
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
     end
   end
 
