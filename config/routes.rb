a883cd35cb78b98c0f5a4dfeed148e59dacdbfb9
sdlong <sdlong.jeng@gmail.com>
2014-07-15 13:29:34 +0800

    rails g controller account::orders  / 建立使用者個人賬戶後台，可以列出所有自己的過去訂單
    作業: 建⽴ account/orders 可以看到該使⽤者過去所有訂單 done

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   root :to => "products#index"
 
   devise_for :users
+  
   namespace :admin do
     resources :products
+  end
+
+  namespace :account do 
+    resources :orders
   end
 
   resources :products do
