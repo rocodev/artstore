ffacb6f154c09da416a7d77557627b0ce54daf36
edcckao <edcckao@gmail.com>
2014-07-21 03:20:07 +0800

    Add account history, and back to history after checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   namespace :admin do 
     resources :products
   end
 
-
+  namespace :account do
+    resources :orders
+  end
+  
   resources :products do 
     member do 
       post :add_to_cart
