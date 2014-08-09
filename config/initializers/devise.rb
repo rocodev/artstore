7e881c2c12d5360753adba444c5a6b5fe2cf1b06
Ryan <hiphubryan@gmail.com>
2014-08-10 00:42:16 +0800

    add devise config.srcret_kry in initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+  #add for production on linode 
+  config.secret_key = 'b4715cc036012ba23dd0149696a4a64e0785c7f443c4ab81731b4d121fa687c75414933a7fda3864da22780931e4db28f84a14d396b26bef5d0977093435eb46'
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
