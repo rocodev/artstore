45cca0e5b567c90b103c42d596440eb8c856be29
Ryan <hiphubryan@gmail.com>
2014-08-11 20:29:21 +0800

    revise logs.cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,16 +34,6 @@ namespace :deploy do
 
 end
 
-# check logs
-namespace :logs do
-  desc "tail rails logs" 
-  task :tail_rails do
-    on roles(:app) do
-      execute "tail -f #{shared_path}/log/#{fetch(:rails_env)}.log"
-    end
-  end
-end
-
 
 
 
