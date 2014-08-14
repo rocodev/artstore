f0054aa746aaac6412cf959ae99b1de2e1d280f2
Ryan <hiphubryan@gmail.com>
2014-08-14 11:02:40 +0800

    comment config.action_controller.asset_host fot test

--- Capfile
+++ Capfile
@@ -1,6 +1,8 @@
 require 'capistrano/console'
-#require 'capistrano/rails/migrations'
-#require 'capistrano/rails/assets'
+
+# this is not set for default
+require 'capistrano/rails/migrations'
+require 'capistrano/rails/assets'
 
 
 # Load DSL and Setup Up Stages
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = 'http://assets0%d.inkryan.com'
+  #config.action_controller.asset_host = 'http://assets0%d.inkryan.com'
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
